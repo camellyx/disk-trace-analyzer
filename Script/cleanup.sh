1e67576ade10534531189e9f83d51e36b4465263
Yixin Luo <camellyx@gmail.com>
2014-11-23 01:32:04 -0500

    more cleanup

--- Script/cleanup.sh
+++ Script/cleanup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+rm -f templates/*[0-9]*.parv
 make -C ../disksim-4.0-ori distclean
 make -C ../disksim-4.0-window distclean
 make -C ../disksim-4.0-prob-promote distclean
+make -C ../disksim-4.0-dynwindow distclean
 
 
